dcf0ab70bf82724a96d7879f9307f3787565d936
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-08-18 19:55:02 +1000

    Update Sharedshit.lua

--- Sharedshit.lua
+++ Sharedshit.lua
@@ -1,5 +1,5 @@
 shared.UiLib = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local m = loadstring(game:HttpGet('https://raw.githubusercontent.com/Tropxzz/Turbo/main/Blacklisted.lua',  true))()
+local m = loadstring(game:HttpGet('https://raw.githubusercontent.com/Tropxzz/Turbo/main/Uilib',  true))()
 shared.Blacklistlist = m.Blacklisted
 
 shared.win = shared.UiLib:CreateWindow({
